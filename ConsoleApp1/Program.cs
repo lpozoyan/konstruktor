d1c11770a46c36c51a6e6238214dfaa1fa615468
stud <stud@PK511-7>
2023-05-02 11:35:18 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿Test test = new Test("hi");
+public class TestBase
+{
+    
+    public TestBase (string m)
+    {
+        Console.WriteLine(" hi im constr 1");
+        Console.WriteLine(m);
+    }
+}
+public class Test : TestBase
+{
+    
+
+    public Test(string m) : base("im constr 2")
+    {
+        Console.WriteLine(" hi im constr 2");
+        Console.WriteLine(m);
+    }
+
+   
+}
